c24d53eec5ad506b2e35d8e9bb3a5cfed730d789
Amelia <ameliappayne@gmail.com>
2020-10-07 00:59:53 -0400

    Event system deallocates events

--- EventSystem.cpp
+++ EventSystem.cpp
@@ -1,22 +1,32 @@
 #include "EventSystem.h"
+#include "System.h"
+#include "Events.h"
 
-
-void EventSystem::subscribe(EventType eventType, System* system) {
+//Get notifications about a given event
+void EventSystem::subscribe(EventType eventType, System* system)
+{
 	//If it dosn't already have that listener add it
-	if (std::count(listeners[eventType].begin(), listeners[eventType].end(), system) == 0) {
+	if (std::count(listeners[eventType].begin(), listeners[eventType].end(), system) == 0)
+	{
 		listeners[eventType].push_back(system);
 	}
 }
 
-void EventSystem::dispatchEvent(Event* theEvent) {
-	for (int i = 0; i < listeners[theEvent->getType()].size(); i++) {
+//Send an event to all listeners
+void EventSystem::dispatchEvent(Event* theEvent)
+{
+	for (int i = 0; i < listeners[theEvent->getType()].size(); i++)
+	{
 		listeners[theEvent->getType()][i]->receiveEvent(theEvent);
 	}
+
+	delete theEvent;
 	
 }
 
 //Remove a given system from receving messages about a given eventtype
-void EventSystem::unSubscribe(EventType eventType, System *system) {
+void EventSystem::unsubscribe(EventType eventType, System *system)
+{
 	auto it = find(listeners[eventType].begin(), listeners[eventType].end(), system);
 	listeners[eventType].erase(it);
 }
--- EventSystem.h
+++ EventSystem.h
@@ -1,18 +1,19 @@
 #include <map>
 #include <vector>
 #include "Enums.h"
-#include "System.h"
-#include "Events.h"
 
-class EventSystem {
+class System;
+class Event;
+
+//Anything with access to the event system can dispatch an event. Only systems can receve events
+class EventSystem
+{
 private:
+	//Map event types to the listeners that care about them
 	std::map<EventType, std::vector<System *> > listeners;
 
 public:
 	void subscribe(EventType eventType, System *system);
-	void unSubscribe(EventType eventType, System *system);
-
+	void unsubscribe(EventType eventType, System *system);
 	void dispatchEvent(Event * theEvent);
-
-
 };
